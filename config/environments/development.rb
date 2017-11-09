954da0bbd38ea62ef431085118155c533f05a59f
badtudou <badtudou@gmail.com>
2017-11-09 15:01:28 +0800

    Enable static assets in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
 
   config.force_ssl = false
 
+  config.serve_static_assets = true
+  config.public_file_server.enabled = true
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
