3c2e1c80b0d7eb189fd40584039f85ccaa21b9ee
badtudou <badtudou@gmail.com>
2017-11-09 12:02:05 +0800

    Set environment to development

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock "~> 3.10.0"
 
 set :application, "kindlenode"
 set :repo_url, "git@github.com:BadTudou/KindleNote-Rails.git"
-set :application, "project_name"
 set :branch, :master
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
@@ -20,7 +19,7 @@ set :puma_conf, -> { "#{shared_path}/puma.rb" }
 set :puma_access_log, -> { "#{shared_path}/log/puma_access.log" }
 set :puma_error_log, -> { "#{shared_path}/log/puma_error.log" }
 set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "development"))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
